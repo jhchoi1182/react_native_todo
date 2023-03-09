1cef2036fa8193e103317811083ff1d1b0dcb96a
jhchoi1182 <jhchoi1182@gmail.com>
2023-03-09 16:34:31 +0900

    중괄호 하나 빠진 것 수정

--- App.js
+++ App.js
@@ -23,120 +23,121 @@ export default function App() {
       alert("저장에 실패했습니다.")
     }
   }
+}
 
-  const loadTodos = async () => {
-    try {
-      const data = await AsyncStorage.getItem(STORAGE_KEY)
-      if (data) setTodos(JSON.parse(data));
-    } catch (error) {
-      alert("데이터를 불러올 수 없습니다.")
-    }
+const loadTodos = async () => {
+  try {
+    const data = await AsyncStorage.getItem(STORAGE_KEY)
+    if (data) setTodos(JSON.parse(data));
+  } catch (error) {
+    alert("데이터를 불러올 수 없습니다.")
   }
+}
 
-  const addTodo = async () => {
-    if (text === "") return
-    const newTodos = { ...todos, [Date.now()]: { text, working } }
-    setTodos(newTodos)
-    await saveTodos(newTodos)
-    setText("")
-  }
+const addTodo = async () => {
+  if (text === "") return
+  const newTodos = { ...todos, [Date.now()]: { text, working } }
+  setTodos(newTodos)
+  await saveTodos(newTodos)
+  setText("")
+}
 
-  const deleteTodo = async (key) => {
-    try {
-      if (Platform.OS === "web") {
-        const ok = confirm("정말 삭제하시겠습니까?")
-        if (ok) {
-          text: "확인", onPress: async () => {
-            const newTodos = { ...todos }
-            delete newTodos[key]
-            setTodos(newTodos)
-            await saveTodos(newTodos)
-          }
+const deleteTodo = async (key) => {
+  try {
+    if (Platform.OS === "web") {
+      const ok = confirm("정말 삭제하시겠습니까?")
+      if (ok) {
+        text: "확인", onPress: async () => {
+          const newTodos = { ...todos }
+          delete newTodos[key]
+          setTodos(newTodos)
+          await saveTodos(newTodos)
         }
-        else {
-          Alert.alert("정말 삭제하시겠습니까?", "확실합니까?", [
-            { text: "취소" },
-            {
-              text: "확인", onPress: async () => {
-                const newTodos = { ...todos }
-                delete newTodos[key]
-                setTodos(newTodos)
-                await saveTodos(newTodos)
-              }
-            }
-          ])
-        }
-
-      } catch (error) {
-        alert("삭제에 실패했습니다.")
       }
+      else {
+        Alert.alert("정말 삭제하시겠습니까?", "확실합니까?", [
+          { text: "취소" },
+          {
+            text: "확인", onPress: async () => {
+              const newTodos = { ...todos }
+              delete newTodos[key]
+              setTodos(newTodos)
+              await saveTodos(newTodos)
+            }
+          }
+        ])
+      }
+
+    } catch (error) {
+      alert("삭제에 실패했습니다.")
     }
+  }
 
   useEffect(() => {
-      loadTodos()
-    }, [])
+    loadTodos()
+  }, [])
 
-    return (
-      <View style={styles.container}>
-        <StatusBar style="light" />
-        <View style={styles.header}>
-          <TouchableOpacity onPress={work}>
-            <Text style={{ ...styles.BtnText, color: working ? "white" : theme.grey }}>일</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={travel}>
-            <Text style={{ ...styles.BtnText, color: !working ? "white" : theme.grey }}>여행</Text>
-          </TouchableOpacity>
-        </View>
-        <TextInput onSubmitEditing={addTodo} value={text} onChangeText={onChangeText} placeholder={working ? "무슨 일을 하고 싶으세요?" : "어디로 여행을 가고 싶으세요?"} style={styles.input} />
-        <ScrollView>{Object.keys(todos).map(key =>
-          todos[key].working === working ?
-            <View style={styles.todo} key={key}>
-              <Text style={styles.todoText}>
-                {todos[key].text}
-              </Text>
-              <TouchableOpacity onPress={() => deleteTodo(key)}>
-                <Fontisto name="trash" size={16} color={theme.grey} />
-              </TouchableOpacity>
-            </View> : null)}</ScrollView>
+  return (
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={work}>
+          <Text style={{ ...styles.BtnText, color: working ? "white" : theme.grey }}>일</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
+          <Text style={{ ...styles.BtnText, color: !working ? "white" : theme.grey }}>여행</Text>
+        </TouchableOpacity>
       </View>
-    );
-  }
+      <TextInput onSubmitEditing={addTodo} value={text} onChangeText={onChangeText} placeholder={working ? "무슨 일을 하고 싶으세요?" : "어디로 여행을 가고 싶으세요?"} style={styles.input} />
+      <ScrollView>{Object.keys(todos).map(key =>
+        todos[key].working === working ?
+          <View style={styles.todo} key={key}>
+            <Text style={styles.todoText}>
+              {todos[key].text}
+            </Text>
+            <TouchableOpacity onPress={() => deleteTodo(key)}>
+              <Fontisto name="trash" size={16} color={theme.grey} />
+            </TouchableOpacity>
+          </View> : null)}</ScrollView>
+    </View>
+  );
+}
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: theme.bg,
-      paddingHorizontal: 45,
-    },
-    header: {
-      justifyContent: "space-between",
-      flexDirection: "row",
-      marginTop: 80
-    },
-    BtnText: {
-      fontSize: 38,
-      fontWeight: 700,
-      color: theme.grey
-    },
-    input: {
-      backgroundColor: "white",
-      paddingVertical: 10,
-      paddingHorizontal: 20,
-      borderRadius: 30,
-      marginVertical: 20,
-      fontSize: 18
-    },
-    todo: {
-      backgroundColor: theme.todoBg,
-      marginVertical: 10,
-      paddingHorizontal: 20,
-      paddingVertical: 20,
-      borderRadius: 15,
-      flexDirection: "row",
-      justifyContent: "space-between"
-    },
-    todoText: {
-      color: "white",
-      fontSize: 16
-    }
-  });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.bg,
+    paddingHorizontal: 45,
+  },
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 80
+  },
+  BtnText: {
+    fontSize: 38,
+    fontWeight: 700,
+    color: theme.grey
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginVertical: 20,
+    fontSize: 18
+  },
+  todo: {
+    backgroundColor: theme.todoBg,
+    marginVertical: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    borderRadius: 15,
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  todoText: {
+    color: "white",
+    fontSize: 16
+  }
+});
